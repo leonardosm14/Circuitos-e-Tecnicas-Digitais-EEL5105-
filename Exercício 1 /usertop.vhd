6764d4c3fb3fc420158ea24d662073fc02d2eb46
Leonardo de Sousa Marques <141475587+leonardosm14@users.noreply.github.com>
2024-03-27 07:57:55 -0300

    Update usertop.vhd

--- Exercício 1 /usertop.vhd
+++ Exercício 1 /usertop.vhd
@@ -2,18 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity usertop is
-port( A: in std_logic;
-      B: in std_logic;
-      C: in std_logic;
-      D: in std_logic;
-      W: out std_logic;
-      X: out std_logic;
-      Y: out std_logic;
-      Z: out std_logic
-	);
+port( A, B, C, D: in std_logic;
+      W, X, Y, Z: out std_logic);
 end usertop;
 
-architecture exercicio of usertop is
+architecture lgc of usertop is
 begin
     
     W <= (not(A) and B and not(C)) or (A and not(B) and not(C)) or (not(B) and c and not(D)) or (not(a) and c);
@@ -21,4 +14,4 @@ begin
     Y <= (not(A) and not(B) and not(C) and not(D)) or (not(A) and not(B) and C and D) or (B and C and not(D)) or (A and B and not(D)) or (A and not(B) and not(C) and D);
     Z <= (not(A) and not(B) and not(C)) or (not(A) and not(B) and not(D));
     
-end exercicio;
+end lgc;
